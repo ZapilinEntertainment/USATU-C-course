cb8359689fa2fbea2f0ab8408b06cb9893ce05c9
ZapilinEntertainment <mahariusls@yandex.ru>
2017-01-08 21:08:20 +0500

    Add files via upload
    fixed range (-128-127) and deleted binary-trying-sort

--- lr3-v14-4.c
+++ lr3-v14-4.c
@@ -10,10 +10,6 @@ int length=0;
 inline void PrintArray (char *m) 
 {
 	int i=0;
-	//for (i;i<length;i++)
-//	{
-//		printf("%c ",m[i]);
-//	}
 	printf ("\n");
 	for (i=0;i<length;i++)
 	{
@@ -40,7 +36,7 @@ void FillArray (char *m,int up,int lowest)
 
 void InsertionDescendingSort(char *m)
 {
-	int i=0,j=0,max=-1,maxpos=0,chosen_pos=1,buffered=0;
+	int i=0,j=0,max=-256,maxpos=0,buffered=0;
 	for (i;i<length-1;i++)
 	{
 		max=m[i];
@@ -62,41 +58,6 @@ void InsertionDescendingSort(char *m)
 	}
 }
 
-void BinaryInsertionDescendingSort (char *arr) 
-{
-	int i,k,middle,right,left;
-	char temp;
-	for (i=1;i<length;i++)
-	{
-		left=0;
-		right=i;		
-		while (right-left>0) 
-		{
-		middle=(left+right)/2;
-		if (arr[i]<arr[middle]) left=middle+1;
-		else 
-		 {
-		 	if (arr[i]==arr[middle]) 
-			 {
-				 left=middle+1;
-				 right=left;
-			 }
-		 	else 
-			 {
-			 	right=middle;
-			 }
-			 
-		 }	
-		}
-		temp=arr[i];
-		for (k=i;k>left;k--)
-		{
-			arr[k]=arr[k-1];
-		}
-		arr[middle+1]=temp;
-	
-	}
-}
 
 
 
@@ -105,12 +66,12 @@ int main(int argc, char *argv[])
 	printf ("set length: ");
 	scanf ("%i",&length);
 	int upper_bound,lower_bound;
-	printf ("(using unsigned char, numbers from 0 to 255)\n");
+	printf ("(using signed char, numbers from -128 to 127)\n");
 	printf ("set lower bound: ");
 	scanf ("%i",&lower_bound);
 	printf ("set upper bound: ");
 	scanf ("%i",&upper_bound);
-	if (lower_bound>upper_bound||upper_bound<0||lower_bound<0||lower_bound>255||upper_bound>255) 
+	if (lower_bound>upper_bound||upper_bound<-128||lower_bound<-128||lower_bound>127||upper_bound>127) 
 	{
 		printf("incorrect input!");
 		_getch();
@@ -120,7 +81,7 @@ int main(int argc, char *argv[])
 	FillArray(m,upper_bound,lower_bound);
 	printf (" unsorted array: \n");
 	PrintArray(m);
-	BinaryInsertionDescendingSort(m);
+	InsertionDescendingSort(m);
 	printf("\n sorted array: \n");
 	PrintArray(m);
 	_getch();
